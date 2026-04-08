a98171e59772b2f77be5218c54a54c644ad12cbd
devaPOC <devat3204@gmail.com>
2026-04-08 19:40:51 +0530

    Refactor templates for consistent formatting and improve URL routing

--- auth_service/urls.py
+++ auth_service/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/users/", include("users.api.urls")),
     path("users/", include("users.urls")),
+    path("", include("users.urls")),
 ]
